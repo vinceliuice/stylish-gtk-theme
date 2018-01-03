e64befeb7418d522368d94cd45413065a7c05eaf
vinceliuice <vinceliuice@hotmail.com>
2018-01-03 17:48:12 +0800

    Update style
    Update style

--- src/gtk-2.0/Stylish/render-assets.sh
+++ src/gtk-2.0/Stylish/render-assets.sh
@@ -5,10 +5,16 @@ OPTIPNG="/usr/bin/optipng"
 
 SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
+
+DARK_SRC_FILE="assets-dark.svg"
+DARK_ASSETS_DIR="assets-Dark"
+LIGHT_ASSETS_DIR="assets-Light"
+
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
-do 
+do
+
 if [ -f $ASSETS_DIR/$i.png ]; then
     echo $ASSETS_DIR/$i.png exists.
 else
@@ -16,9 +22,43 @@ else
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-background-opacity=0 \
               --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
     && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
 fi
+
+if [ -f $DARK_ASSETS_DIR/$i.png ]; then
+    echo $DARK_ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $DARK_ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-png=$DARK_ASSETS_DIR/$i.png $DARK_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $DARK_ASSETS_DIR/$i.png 
+fi
 done
+
+cp $ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar.png
+cp $ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar.png
+cp $ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar.png
+
+cp $ASSETS_DIR/menubar.png menubar-toolbar/menubar.png
+cp $ASSETS_DIR/menubar-light.png menubar-toolbar/menubar-light.png
+cp $ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button.png
+
+
+cp $DARK_ASSETS_DIR/button.png menubar-toolbar/button.png
+cp $DARK_ASSETS_DIR/button-hover.png menubar-toolbar/button-hover.png
+cp $DARK_ASSETS_DIR/button-active.png menubar-toolbar/button-active.png
+cp $DARK_ASSETS_DIR/button-insensitive.png menubar-toolbar/button-insensitive.png
+
+cp $DARK_ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar-dark.png
+cp $DARK_ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar-dark.png
+cp $DARK_ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar-dark.png
+
+cp $DARK_ASSETS_DIR/menubar.png menubar-toolbar/menubar-dark.png
+cp $DARK_ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button-dark.png
+
+cp $ASSETS_DIR/*.png $LIGHT_ASSETS_DIR/
+
 exit 0
